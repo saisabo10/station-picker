ce899b2b1931dda87ee1aee1b67a949e82a6682e
saisabo10 <149946815+saisabo10@users.noreply.github.com>
2024-07-05 22:06:42 +0900

    Update toei.js

--- assets/toei/toei.js
+++ assets/toei/toei.js
@@ -432,5 +432,59 @@ const toeiStations = {
     },
   ],
 };
-
+  "nippori-toneri-liner": [
+    { 
+      name: "日暮里",
+      id: "nippori",
+    },
+    {
+      name: "西日暮里",
+      id: "nishi-nippori",
+    },
+    {
+      name: "赤土小学校前",
+      id: "akado-shogakko-mae",
+    },
+    {
+      name: "熊野前",
+      id: "kumanomae",
+    },
+    {
+      name: "足立小台",
+      id: "adachi-odai",
+    },
+    {
+      name: "扇大橋",
+      id: "oji-ohashi",
+    },
+    {
+      name: "高野",
+      id: "koya",
+    },
+    {
+      name: "江北",
+      id: "kohoku",
+    },
+    {
+      name: "西新井大師西",
+      id: "nishiaraidaishinishi",
+    },
+    {
+      name: "谷在家",
+      id: "yazaike",
+    },
+    {
+      name: "舎人公園",
+      id: "toneri-koen",
+    },
+    {
+      name: "舎人",
+      id: "toneri",
+    },
+    {
+      name: "見沼代親水公園",
+      id: "minumadai-shinsui-koen",
+    },
+  ],
+};
 generateRailwayStations("toei", toeiStations);
