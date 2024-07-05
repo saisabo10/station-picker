42a755442aae8556ef80db1680526019bd13f01c
saisabo10 <149946815+saisabo10@users.noreply.github.com>
2024-07-05 22:26:12 +0900

    Create minato.js

---
+++ assets/hitatinakakaihin/minato.js
@@ -0,0 +1,42 @@
+const hitachinakaStations = {
+  "hitachinaka-minato": [
+    { 
+      name: "勝田",
+      id: "katsuta"
+    },
+    {
+      name: "日工前",
+      id: "nikkomae"
+    },
+    {
+      name: "金上",
+      id: "kanaage"
+    },
+    {
+      name: "中根",
+      id: "nakane"
+    },
+    {
+      name: "高田の鉄橋",
+      id: "takadanotekkyo"
+    },
+    {
+      name: "平磯",
+      id: "hiraiso"
+    },
+    {
+      name: "殿山",
+      id: "tonoyama"
+    },
+    {
+      name: "磯崎",
+      id: "isozaki"
+    },
+    {
+      name: "阿字ヶ浦",
+      id: "ajigaura"
+    }
+  ]
+};
+
+generateRailwayStations("hitachinaka", hitachinakaStations);
