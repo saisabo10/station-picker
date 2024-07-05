ef763b8534f8331f50fbbb7eb5ad1c79459593aa
saisabo10 <149946815+saisabo10@users.noreply.github.com>
2024-07-05 22:17:08 +0900

    Create kantetu.js

---
+++ assets/kantetu/kantetu.js
@@ -0,0 +1,118 @@
+const ryugasakiLineStations = {
+  "ryugasaki-line": [
+    { 
+      name: "佐貫",
+      id: "sanuki",
+    },
+    {
+      name: "入地",
+      id: "irichi",
+    },
+    {
+      name: "竜ヶ崎",
+      id: "ryugasaki",
+    },
+  ],
+};
+generateRailwayStations("ryugasaki", ryugasakiLineStations);
+const josoLineStations = {
+  "kanto-joso": [
+    { 
+      name: "取手",
+      id: "toride",
+    },
+    {
+      name: "寺原",
+      id: "terahara",
+    },
+    {
+      name: "新取手",
+      id: "shintoride",
+    },
+    {
+      name: "ゆめみ野",
+      id: "yumemino",
+    },
+    {
+      name: "稲戸井",
+      id: "inadodai",
+    },
+    {
+      name: "戸頭",
+      id: "togashira",
+    },
+    {
+      name: "南守谷",
+      id: "minamimoriya",
+    },
+    {
+      name: "守谷",
+      id: "moriya",
+    },
+    {
+      name: "新守谷",
+      id: "shinmoriya",
+    },
+    {
+      name: "小絹",
+      id: "kokinu",
+    },
+    {
+      name: "水海道",
+      id: "mizukaido",
+    },
+    {
+      name: "北水海道",
+      id: "kitamizukaido",
+    },
+    {
+      name: "中妻",
+      id: "nakatsuma",
+    },
+    {
+      name: "三妻",
+      id: "mitsuma",
+    },
+    {
+      name: "南石下",
+      id: "minamiishige",
+    },
+    {
+      name: "石下",
+      id: "ishige",
+    },
+    {
+      name: "玉村",
+      id: "tamamura",
+    },
+    {
+      name: "宗道",
+      id: "sodo",
+    },
+    {
+      name: "下妻",
+      id: "shimotsuma",
+    },
+    {
+      name: "大宝",
+      id: "daiho",
+    },
+    {
+      name: "騰波ノ江",
+      id: "tonami",
+    },
+    {
+      name: "黒子",
+      id: "kurogo",
+    },
+    {
+      name: "大田郷",
+      id: "otago",
+    },
+    {
+      name: "下館",
+      id: "shimodate",
+    },
+  ],
+};
+generateRailwayStations("kanto", josoLineStations);
