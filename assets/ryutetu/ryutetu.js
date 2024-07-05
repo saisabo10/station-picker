bc2f91214392b64bf2ccc956a52dadf958f6aa7d
saisabo10 <149946815+saisabo10@users.noreply.github.com>
2024-07-05 22:10:46 +0900

    Create ryutetu.js

---
+++ assets/ryutetu/ryutetu.js
@@ -0,0 +1,30 @@
+const ryutetsuStations = {
+  "ryutetsu-line": [
+    { 
+      name: "馬橋",
+      id: "mabashi",
+    },
+    {
+      name: "幸谷",
+      id: "koya",
+    },
+    {
+      name: "小金城趾",
+      id: "koganejoushi",
+    },
+    {
+      name: "鰭ケ崎",
+      id: "himegasaki",
+    },
+    {
+      name: "平和台",
+      id: "heiwadai",
+    },
+    {
+      name: "流山",
+      id: "nagareyama",
+    },
+  ],
+};
+
+generateRailwayStations("ryutetsu", ryutetsuStations);
