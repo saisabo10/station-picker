347d358761df5a0a9a03ddca75d9d27ed300d40d
saisabo10 <149946815+saisabo10@users.noreply.github.com>
2024-07-05 22:21:45 +0900

    Create kasimarinkai.js

---
+++ assets/kashimarinkai/kasimarinkai.js
@@ -0,0 +1,66 @@
+const oaraiKashimaLineStations = {
+  "oarai-kashima": [
+    { 
+      name: "水戸",
+      id: "mito",
+    },
+    { 
+      name: "東水戸",
+      id: "higashi-mito",
+    },
+    { 
+      name: "常澄",
+      id: "tsunezumi",
+    },
+    { 
+      name: "大洗",
+      id: "oarai",
+    },
+    { 
+      name: "涸沼",
+      id: "karinuma",
+    },
+    { 
+      name: "鹿島旭",
+      id: "kashima-asahi",
+    },
+    { 
+      name: "徳宿",
+      id: "tokujuuku",
+    },
+    { 
+      name: "新鉾田",
+      id: "shin-hokota",
+    },
+    { 
+      name: "北浦湖畔",
+      id: "kitaura-kohan",
+    },
+    { 
+      name: "大洋",
+      id: "taiyo",
+    },
+    { 
+      name: "鹿島灘",
+      id: "kashimanada",
+    },
+    { 
+      name: "鹿島大野",
+      id: "kashima-ono",
+    },
+    { 
+      name: "長者ヶ浜潮騒はまなす公園前",
+      id: "chojagahama-shiosai-hamanasu-koenmae",
+    },
+    { 
+      name: "荒野台",
+      id: "koyadai",
+    },
+    { 
+      name: "鹿島サッカースタジアム",
+      id: "kashima-soccer-stadium",
+    },
+  ],
+};
+
+generateRailwayStations("oarai-kashima", oaraiKashimaLineStations);
